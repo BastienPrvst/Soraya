0c34200b2da1f73ea6184635ac9420cb38865990
Bastien <a@a.a>
2026-09-10 15:22:22 +0200

    Finition page panier vide

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -19,7 +19,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
-use App\Controller\Admin\Fields\CKEditorField;
 
 class ProductCrudController extends AbstractCrudController
 {
